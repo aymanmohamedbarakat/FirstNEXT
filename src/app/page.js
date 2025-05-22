79644f735af6a0218eeef2dc3b4ee43b32886100
aymanmohamedbarakat <aymanmohamedbarakat@gmail.com>
2025-05-22 17:25:59 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='text-5xl bg-amber-900 text-amber-100'>Hello World !</div>
+    <div className='text-5xl bg-blue-900 text-amber-100'>Hello World !</div>
   )
 }
 
